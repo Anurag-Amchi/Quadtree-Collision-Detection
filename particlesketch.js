ac287e2c3e8e0ea2aba66de79de8daaceb44a1c7
Anurag Amchi <112432737+Anurag-Amchi@users.noreply.github.com>
2024-06-04 18:47:24 +0530

    Update particlesketch.js
    Added sketch for both O(n^2) simulation and O(nlog(n)) simulation by Quadtree

--- particlesketch.js
+++ particlesketch.js
@@ -1,1 +1,63 @@
+// let particles = [];
 
+// function setup(){
+//     createCanvas(600,400);
+//     for (let i=0;i<1000;i++){
+//         particles[i] = new Particle(random(width),random(height));
+//     }
+// }
+
+// function draw(){
+//     background(0);
+//     for (let p of particles){
+//         p.move();
+//         p.render();
+//         p.bounceOffEdges();
+//         p.setHighlight(false);
+//     }
+
+//     for(let p of particles) {
+//         for (let other of particles){
+//             if(p!== other && p.intersects(other)){
+//                 p.setHighlight(true);
+//             }
+//         }
+//     }
+// }
+
+// Above is the O(n^2) method of collision detection(very slow)
+
+let particles = [];
+
+function setup(){
+    createCanvas(600,400);
+    for (let i=0;i<1000;i++){
+        particles[i] = new Particle(random(width),random(height));
+    }
+}
+
+function draw(){
+    background(0);
+    let boundary = new Rectangle(300,200,300,200);
+    let qtree = new QuadTree(boundary,4);
+    
+    for (let p of particles){
+        let point = new Point(p.position.x, p.position.y, p);
+        qtree.insert(point);
+        p.move();
+        p.render();
+        p.bounceOffEdges();
+        p.setHighlight(false);
+    }
+    
+    for(let p of particles) {
+        let range = new Circle(p.position.x, p.position.y, p.size * 2);
+        let points = qtree.query(range)
+        for (let pt of points){
+            let other = pt.userData;
+            if(p!== other && p.intersects(other)){
+                p.setHighlight(true);
+            }
+        }
+    }
+}
