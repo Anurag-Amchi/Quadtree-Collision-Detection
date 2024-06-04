5c4edac043ba37c13db5ad43636490f3d311721a
Anurag Amchi <112432737+Anurag-Amchi@users.noreply.github.com>
2024-06-04 18:50:19 +0530

    Update quadtree.js
    Added userdata to class Point and added the Circle class.

--- quadtree.js
+++ quadtree.js
@@ -1,7 +1,9 @@
 class Point {
-    constructor(x,y){
+    constructor(x, y, userData) {
         this.x = x;
         this.y = y;
+        // collision detection
+        this.userData = userData;
     }
 }
 
@@ -32,6 +34,48 @@ class Rectangle{
             range.x - range.w <= this.x - this.w &&
             range.y + range.h >= this.y + this.h &&
             range.y - range.h <= this.y - this.h);
+    }
+}
+
+//for collision detection
+class Circle {
+    constructor(x, y, r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.rSquared = this.r * this.r;
+    }
+
+    contains(point) {
+        // check if the point is in the circle by checking if the euclidean distance of
+        // the point and the center of the circle if smaller or equal to the radius of
+        // the circle
+        let d = Math.pow((point.x - this.x), 2) + Math.pow((point.y - this.y), 2);
+        return d <= this.rSquared;
+    }
+    intersects(range) {
+
+        let xDist = Math.abs(range.x - this.x);
+        let yDist = Math.abs(range.y - this.y);
+
+        // radius of the circle
+        let r = this.r;
+
+        let w = range.w / 2;
+        let h = range.h / 2;
+
+        let edges = Math.pow((xDist - w), 2) + Math.pow((yDist - h), 2);
+
+        // no intersection
+        if (xDist > (r + w) || yDist > (r + h))
+            return false;
+
+        // intersection within the circle
+        if (xDist <= w || yDist <= h)
+            return true;
+
+        // intersection on the edge of the circle
+        return edges <= this.rSquared;
     }
 }
 
